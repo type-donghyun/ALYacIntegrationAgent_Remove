e6b34f335437876d23da654f9a0d4bf2ec3b3e9d
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2023-03-09 11:24:59 +0900

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,23 +1,28 @@
 @ECHO OFF
 ::================================================================================관리자 권한 요청
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-IF %errorlevel% NEQ 0 (
-	ECHO Requesting administrative privileges...
-	GOTO UACPrompt
-) ELSE (
-	GOTO gotAdmin
-)
+:: BatchGotAdmin
+ :-------------------------------------
+ REM  --> Check for permissions
+ >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+ if '%errorlevel%' NEQ '0' (
+     echo Requesting administrative privileges...
+     goto UACPrompt
+ ) else ( goto gotAdmin )
 
 :UACPrompt
-ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-ECHO UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-EXIT /B
+     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+     exit /B
 
 :gotAdmin
-IF EXIST "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-PUSHD "%CD%"
-CD /D "%~dp0"
+     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+     pushd "%CD%"
+     CD /D "%~dp0"
+ :--------------------------------------
 ::====================================================================================================
 
 ::================================================================================ECHO 색상 설정
