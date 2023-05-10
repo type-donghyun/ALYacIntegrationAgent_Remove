225747a5ad1fc055ad86b6c28c13a36ca7476bfc
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2023-05-10 16:13:25 +0900

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -23,44 +23,19 @@ IF %errorlevel% neq 0 (
 	CD /D "%~dp0"
 
 ::_____________________________________________________________________________________________________________________________________________________________
-:: ECHO 색상 설정
-
-SET _elev=
-IF /i "%~1"=="-el" SET _elev=1
-FOR /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
-SET "_null=1>nul 2>nul"
-SET "_psc=powershell"
-SET "EchoBlack=%_psc% write-host -back DarkGray -fore Black"
-SET "EchoBlue=%_psc% write-host -back Black -fore DarkBlue"
-SET "EchoGreen=%_psc% write-host -back Black -fore Darkgreen"
-SET "EchoCyan=%_psc% write-host -back Black -fore DarkCyan"
-SET "EchoRed=%_psc% write-host -back Black -fore DarkRed"
-SET "EchoPurple=%_psc% write-host -back Black -fore DarkMagenta"
-SET "EchoYellow=%_psc% write-host -back Black -fore DarkYellow"
-SET "EchoWhite=%_psc% write-host -back Black -fore Gray"
-SET "EchoGray=%_psc% write-host -back Black -fore DarkGray"
-SET "EchoLightBlue=%_psc% write-host -back Black -fore Blue"
-SET "EchoLightGreen=%_psc% write-host -back Black -fore Green"
-SET "EchoLightCyan=%_psc% write-host -back Black -fore Cyan"
-SET "EchoLightRed=%_psc% write-host -back Black -fore Red"
-SET "EchoLightPurple=%_psc% write-host -back Black -fore Magenta"
-SET "EchoLightYellow=%_psc% write-host -back Black -fore Yellow"
-SET "EchoBrightWhite=%_psc% write-host -back Black -fore White"
-
-::_____________________________________________________________________________________________________________________________________________________________
 
 CHCP 65001 > nul
 TITLE 알약 제거
 
 ECHO 알약 통합에이전트 제거를 위해 안전모드로 다시 시작해야합니다.
 ECHO 다시 시작 시, 실행되고 있는 모든 프로그램이 강제로 종료됩니다.
-%EchoYellow% 다시 시작 후, 바탕 화면의 [저를 실행해주세요!]를 실행해주세요.
+ECHO 다시 시작 후, 바탕 화면의 [저를 실행해주세요!]를 실행해주세요.
 CHOICE /c 12 /n /t 10 /d 2 /m "안전모드로 변경 후 다시 시작하시겠습니까? [1] Yes, [2] No"
 
 CLS
 IF %errorlevel% equ 1 (
 	BCDEDIT /set {current} safeboot minimal > nul
-	COPY "bat\ALYacIntegrationAgent_Remove.bat" "%UserProfile%\desktop\저를 실행해주세요!.bat" > nul
+	COPY "temp\ALYacIntegrationAgent_Remove.bat" "%UserProfile%\desktop\저를 실행해주세요!.bat" > nul
 	SHUTDOWN /r /t 3 /c "안전모드로 다시 시작합니다." /f
 ) ELSE IF %errorlevel% equ 2 (
 	ECHO 작업을 취소합니다.
